a1cf408c480979bb6de140097afdf7b99f29fdd1
HanDong Wang <handongwang@HanDongdeMacBook-Pro.local>
2018-01-12 20:12:18 +0800

    add source

---
+++ library/Business1JumpHandle.h
@@ -0,0 +1,14 @@
+//
+//  Business1JumpHandle.h
+//  ChieryBusiness1
+//
+//  Created by chiery on 2018/1/8.
+//  Copyright © 2018年 com.chiery.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ChieryJumpHandlePrt.h"
+
+@interface Business1JumpHandle : NSObject <ChieryJumpHandlePrt>
+
+@end
---
+++ library/ChieryBusiness1.h
@@ -0,0 +1,13 @@
+//
+//  ChieryBusiness1.h
+//  ChieryBusiness1
+//
+//  Created by chiery on 2018/1/8.
+//  Copyright © 2018年 com.chiery.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChieryBusiness1 : UIViewController
+
+@end
